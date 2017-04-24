ace59b0ead285edadf1ea3558d298ed8317346b2
Laura Santamaria <laura.santamaria@rackspace.com>
2017-04-24 17:27:54 -0500

    starting to get started on the envelope engine

--- ramlpreparer/builders/htmlEnvelope.py
+++ ramlpreparer/builders/htmlEnvelope.py
@@ -3,6 +3,8 @@
 # TODO: Replace psuedocode with real code
 
 from bs4 import BeautifulSoup
+import tocbuilder
+import json
 
 # Take in HTML
 originalHTML = array_with_html_pages
@@ -14,6 +16,16 @@ for each_page in originalHTML:
     create an empty JSON envelope (instance of class)
     write soupit.find(div='col-md-9') to body field in envelope
     write soupit.title.string to title tag
-    parse body for headings
-    convert headings into layered toc div
-    put toc div in toc tag
+    toc_html_envelope = htmlify(each_page)
+
+whole_enchilada = {
+                    'body': body_html_envelope
+                    'title': title_envelope
+                    'author': author_envelope
+                    'toc': toc_html_envelope
+                    'publish_date': pubdate_envelope
+                    'categories': cat_envelope
+                    'unsearchable': deconst_json_search_choice
+}
+
+print(json.dumps(whole_enchilada))
