2693916567b69ad09e2d06b487780554a2f1075c
Laura Santamaria <laura.santamaria@rackspace.com>
2017-04-25 17:45:51 -0500

    working with ramlficatins to build the HTML. Starting work

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 #TODO
 -   Get the coding done...
 -   Remove copy-pasta stuff
+-   Add a way to add intro text or other information to the generated docs (maybe a way to pull in markdown? straight HTML?)
--- ramlpreparer/builders/raml2html.py
+++ ramlpreparer/builders/raml2html.py
@@ -1,7 +1,38 @@
 #! /usr/bin/env python
 
+import ramlfications
 # TODO: Replace psuedocode
 
-# Retrieve the RAML
-# Install raml2html converter
-# Run raml2html converter
+api_doc = "/Users/laur0616/testRAMLcase/api.raml"
+apiDict = {}
+resourceList = []
+n = 0
+
+parsed = ramlfications.parse(api_doc)
+
+apiDict['title'] = parsed.title
+apiDict['version'] = parsed.version
+apiDict['documentation'] = parsed.documentation
+apiDict['baseUri'] = parsed.base_uri
+
+for item in parsed.resources:
+    resourceList.append(item)
+
+for item in resourceList:
+    apiDict_sub = {}
+    apiDict_sub['name'] = item.name
+    apiDict_sub['display_name'] = item.display_name
+    apiDict_sub['description'] = item.description
+    apiDict_sub['methods'] = item.method
+    apiDict_sub['URI'] = item.uri_params
+    apiDict_sub['responses'] = item.responses
+    apiDict[n] = apiDict_sub
+    n += 1
+
+print(apiDict)
+
+# <div id="title">
+#   <h2>display_name</h2>
+#   <div class='code'></div>
+#   <p>description</p>
+#
