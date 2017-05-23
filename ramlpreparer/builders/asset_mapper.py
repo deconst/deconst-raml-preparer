be47b7e84a7313818b0d5076733ad6531ec9c1ae
Laura Santamaria <laura.santamaria@rackspace.com>
2017-05-23 16:41:11 -0500

    getting the system to find the asset offsets in the html file working

--- ramlpreparer/builders/asset_mapper.py
+++ ramlpreparer/builders/asset_mapper.py
@@ -1,25 +1,37 @@
 #! usr/bin/env python
 
 import os
+import re
+from bs4 import BeautifulSoup
 
-# Take the assets and copy them over.
-# Find all assets in the HTML.
+# TODO: Step 1: Take the assets and copy them over.
 
-the_body = soup.body
+
+# DONE: Step 2: Find all assets in the HTML.
+
+with open(os.getcwd() + '/tests/tester-raw.html', 'r') as html_doc_sample:
+    soup = BeautifulSoup(html_doc_sample, 'html.parser')
+
 asset_offset_envelope = {}
+element1 = str(soup)
 
-# TODO: Figure out how to find it in the_body so this for loop works to find
-# the offset...
-for img in the_body:
-    n = str(img)
-    asset_offset_envelope[n] = [the_body.ascii_lowercase.index('<img')]
+for img in soup.find_all('img'):
+    tag_string = str(img)
+    begin_offset = element1.index(tag_string)
+    end_offset = len(tag_string) + begin_offset
+    n = (begin_offset, end_offset)
+    print(n)
+    asset_offset_envelope[n] = [tag_string]
 
-# Map all assets in the HTML to the location in the asset directory.
+print(asset_offset_envelope)
+
+# Step 3: Map all assets in the HTML to the location in the asset directory.
 # TODO: Replace pseudocode.
-for asset in original_asset_dir:
-    copy image to ASSET_DIR
-    get path using os.path()
-    find the match in the asset_offset_envelope
-    append path to list of the match
+# for asset in original_asset_dir:
+#     copy image to ASSET_DIR
+#     get path using os.path()
+#     find the match in the asset_offset_envelope
+#     append path to list of the match
 
-# TODO: Replace the asset in the HTML with the single-character placehoolder.
+# TODO: Step 4: Replace the asset in the HTML with the single-character
+# placehoolder.
