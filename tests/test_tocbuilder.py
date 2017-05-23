3e6d027dfcd5ac2e2e5adcf516327668e4b8d2d4
Laura Santamaria <laura.santamaria@rackspace.com>
2017-05-23 10:46:44 -0500

    cleaning this one up and adding a bug note. Will get back to it later.

--- tests/test_tocbuilder.py
+++ tests/test_tocbuilder.py
@@ -63,7 +63,8 @@ class TocBuilderTestCase(unittest.TestCase):
         '''
         soup = BeautifulSoup('<h3>test</h3><h3>test</h3>', 'html.parser')
         the_method = sibs_it(
-            soup.h3, 'h3', ['current_heading_list'], re.compile('h[1,2,3]'), ['toc_builder'])
+            soup.h3, 'h3', ['current_heading_list'], re.compile(
+                'h[1,2,3]'), ['toc_builder'])
         self.assertEqual(
             (['toc_builder'], ['current_heading_list'], None, '3'), the_method)
 
@@ -74,9 +75,11 @@ class TocBuilderTestCase(unittest.TestCase):
         '''
         soup = BeautifulSoup('<h3>test</h3><h2>test</h2>', 'html.parser')
         the_method = sibs_it(
-            soup.h3, 'h3', ['current_heading_list'], re.compile('h[1,2,3]'), ['toc_builder'], ['h2 list'])
+            soup.h3, 'h3', ['current_heading_list'], re.compile(
+                'h[1,2,3]'), ['toc_builder'], ['h2 list'])
         self.assertEqual(
-            (['toc_builder'], [], ['h2 list', ['current_heading_list']], '2'), the_method)
+            (['toc_builder'], [], ['h2 list', ['current_heading_list']], '2'),
+            the_method)
 
     def test_sibs_h3_followed_by_h1(self):
         '''
@@ -85,9 +88,11 @@ class TocBuilderTestCase(unittest.TestCase):
         '''
         soup = BeautifulSoup('<h3>test</h3><h1>test</h1>', 'html.parser')
         the_method = sibs_it(
-            soup.h3, 'h3', ['current_heading_list'], re.compile('h[1,2,3]'), ['toc_builder'])
+            soup.h3, 'h3', ['current_heading_list'], re.compile(
+                'h[1,2,3]'), ['toc_builder'])
         self.assertEqual(
-            (['toc_builder', ['current_heading_list']], [], None, '1'), the_method)
+            (['toc_builder', ['current_heading_list']],
+             [], None, '1'), the_method)
 
     def test_parse_it_h1_only(self):
         '''
@@ -107,9 +112,18 @@ class TocBuilderTestCase(unittest.TestCase):
         Does parse_it work for H1 and H2 tags?
         '''
         self.maxDiff = None
-        html_sample = '<body><h1>Heading 1 h2</h1><h2>Heading 1.1 h2</h2><h2>Heading 1.2 h2</h2><h1>Heading 2 h2</h1><h2>Heading 2.1 h2</h2><h2>Heading 2.2 h2</h2></body>'
-        the_result = ['<li><a href="#Heading1h2">Heading 1 h2</a></li>', [['<li><a href="#Heading1.1h2">Heading 1.1 h2</a></li>', '<li><a href="#Heading1.2h2">Heading 1.2 h2</a></li>']],
-                      '<li><a href="#Heading2h2">Heading 2 h2</a></li>', ['<li><a href="#Heading2.1h2">Heading 2.1 h2</a></li>', '<li><a href="#Heading2.2h2">Heading 2.2 h2</a></li>']]
+        html_sample = (
+            '<body><h1>Heading 1 h2</h1><h2>Heading 1.1 h2</h2>'
+            '<h2>Heading 1.2 h2</h2><h1>Heading 2 h2</h1>'
+            '<h2>Heading 2.1 h2</h2><h2>Heading 2.2 h2</h2></body>')
+        # BUG: Need to figure out why the double square bracket appears on the
+        # first 2nd level here.
+        the_result = ['<li><a href="#Heading1h2">Heading 1 h2</a></li>', [
+            ['<li><a href="#Heading1.1h2">Heading 1.1 h2</a></li>',
+             '<li><a href="#Heading1.2h2">Heading 1.2 h2</a></li>']],
+            '<li><a href="#Heading2h2">Heading 2 h2</a></li>', [
+                '<li><a href="#Heading2.1h2">Heading 2.1 h2</a></li>',
+                '<li><a href="#Heading2.2h2">Heading 2.2 h2</a></li>']]
         the_method = parse_it(html_sample)
         self.assertEqual(the_method, the_result)
 
@@ -118,26 +132,67 @@ class TocBuilderTestCase(unittest.TestCase):
         Does parse_it work for H1, H2, and H3 tags?
         '''
         self.maxDiff = None
-        html_sample = '<body><h1>Heading 1 h3</h1><h2>Heading 1.1 h3</h2><h3>Heading 1.1.1 h3</h3><h3>Heading 1.1.2 h3</h3><h2>Heading 1.2 h3</h2><h3>Heading 1.2.1 h3</h3><h1>Heading 2 h3</h1><h2>Heading 2.1 h3</h2><h2>Heading 2.2 h3</h2></body>'
-        the_result = ['<li><a href="#Heading1h3">Heading 1 h3</a></li>', ['<li><a href="#Heading1.1h3">Heading 1.1 h3</a></li>', ['<li><a href="#Heading1.1.1h3">Heading 1.1.1 h3</a></li>', '<li><a href="#Heading1.1.2h3">Heading 1.1.2 h3</a></li>'],
-                                                                          '<li><a href="#Heading1.2h3">Heading 1.2 h3</a></li>', ['<li><a href="#Heading1.2.1h3">Heading 1.2.1 h3</a></li>']], '<li><a href="#Heading2h3">Heading 2 h3</a></li>', ['<li><a href="#Heading2.1h3">Heading 2.1 h3</a></li>', '<li><a href="#Heading2.2h3">Heading 2.2 h3</a></li>']]
+        html_sample = (
+            '<body><h1>Heading 1 h3</h1><h2>Heading 1.1 h3</h2>'
+            '<h3>Heading 1.1.1 h3</h3><h3>Heading 1.1.2 h3</h3>'
+            '<h2>Heading 1.2 h3</h2><h3>Heading 1.2.1 h3</h3>'
+            '<h1>Heading 2 h3</h1><h2>Heading 2.1 h3</h2>'
+            '<h2>Heading 2.2 h3</h2></body>')
+        the_result = ['<li><a href="#Heading1h3">Heading 1 h3</a></li>', [
+            '<li><a href="#Heading1.1h3">Heading 1.1 h3</a></li>', [
+                '<li><a href="#Heading1.1.1h3">Heading 1.1.1 h3</a></li>',
+                '<li><a href="#Heading1.1.2h3">Heading 1.1.2 h3</a></li>'],
+            '<li><a href="#Heading1.2h3">Heading 1.2 h3</a></li>', [
+                '<li><a href="#Heading1.2.1h3">Heading 1.2.1 h3</a></li>']],
+            '<li><a href="#Heading2h3">Heading 2 h3</a></li>', [
+                '<li><a href="#Heading2.1h3">Heading 2.1 h3</a></li>',
+            '<li><a href="#Heading2.2h3">Heading 2.2 h3</a></li>']]
         the_method = parse_it(html_sample)
         self.assertEqual(the_method, the_result)
 
-    def test_parse_it_fail(self):
+    def test_parse_it_full_pass(self):
         '''
-        Question?
+        Does parse_it work for a full sample?
         '''
-        html_doc = '<h1>text</h1><h2>text</h2><h3>text</h3>'
-        pass
+        self.maxDiff = None
+        html_sample = (
+            '<body><h1>Heading 1 h3</h1><p>Random text</p>'
+            '<h2>Heading 1.1 h3</h2><p>Random text</p><p>Random text</p>'
+            '<h3>Heading 1.1.1 h3</h3><p>Random text</p><p>Random text</p>'
+            '<h3>Heading 1.1.2 h3</h3><p>Random text</p>'
+            '<h2>Heading 1.2 h3</h2><p>Random text</p><p>Random text</p>'
+            '<h3>Heading 1.2.1 h3</h3><p>Random text</p>'
+            '<h1>Heading 2 h3</h1><p>Random text</p><p>Random text</p>'
+            '<h2>Heading 2.1 h3</h2><p>Random text</p>'
+            '<h2>Heading 2.2 h3</h2><p>Random text</p><p>Random text</p>'
+            '<p>Random text</p></body>')
+        the_result = ['<li><a href="#Heading1h3">Heading 1 h3</a></li>', [
+            '<li><a href="#Heading1.1h3">Heading 1.1 h3</a></li>', [
+                '<li><a href="#Heading1.1.1h3">Heading 1.1.1 h3</a></li>',
+                '<li><a href="#Heading1.1.2h3">Heading 1.1.2 h3</a></li>'],
+            '<li><a href="#Heading1.2h3">Heading 1.2 h3</a></li>', [
+                '<li><a href="#Heading1.2.1h3">Heading 1.2.1 h3</a></li>']],
+            '<li><a href="#Heading2h3">Heading 2 h3</a></li>', [
+                '<li><a href="#Heading2.1h3">Heading 2.1 h3</a></li>',
+            '<li><a href="#Heading2.2h3">Heading 2.2 h3</a></li>']]
+        the_method = parse_it(html_sample)
+        self.assertEqual(the_method, the_result)
+
+    # def test_parse_it_fail(self):
+    #     '''
+    #     Question?
+    #     '''
+    #     html_doc = '<h1>text</h1><h2>text</h2><h3>text</h3>'
+    #     pass
 
     def test_htmlify_pass(self):
         '''
         Does htmlify return a list wrapped in <ul></ul>?
         '''
-        the_list = ['<li>item1</li>', ['<li>sub1</li>', '<li>sub2</li>',
-                                       ['<li>subsub1</li>', '<li>subsub2</li>']],
-                    '<li>item2</li>']
+        the_list = ['<li>item1</li>', [
+            '<li>sub1</li>', '<li>sub2</li>',
+            ['<li>subsub1</li>', '<li>subsub2</li>']],
+            '<li>item2</li>']
         the_result = ['<ul>', '<li>item1</li>',
                       ['<ul>', '<li>sub1</li>', '<li>sub2</li>',
                        ['<ul>', '<li>subsub1</li>', '<li>subsub2</li>',
@@ -146,13 +201,13 @@ class TocBuilderTestCase(unittest.TestCase):
         the_method = htmlify(the_list)
         self.assertEqual(the_method, the_result)
 
-    def test_htmlify_fail(self):
-        '''
-        Question?
-        '''
-        soup = BeautifulSoup(
-            '<h1>text</h1><h2>text</h2><h3>text</h3>', 'html.parser')
-        pass
+    # def test_htmlify_fail(self):
+    #     '''
+    #     Question?
+    #     '''
+    #     soup = BeautifulSoup(
+    #         '<h1>text</h1><h2>text</h2><h3>text</h3>', 'html.parser')
+    #     pass
 
 
 if __name__ == '__main__':
