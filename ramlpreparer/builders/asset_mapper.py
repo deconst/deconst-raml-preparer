7acfa69048c6948e9fd501707a867425b0cd0d5e
Laura Santamaria <laura.santamaria@rackspace.com>
2017-05-24 09:59:58 -0500

    working on the asset mapper. got it doing the index and single chara replacement

--- ramlpreparer/builders/asset_mapper.py
+++ ramlpreparer/builders/asset_mapper.py
@@ -13,17 +13,23 @@ with open(os.getcwd() + '/tests/tester-raw.html', 'r') as html_doc_sample:
     soup = BeautifulSoup(html_doc_sample, 'html.parser')
 
 asset_offset_envelope = {}
+changed_envelope = {}
 element1 = str(soup)
+element2 = 0
 
 for img in soup.find_all('img'):
     tag_string = str(img)
     begin_offset = element1.index(tag_string)
     end_offset = len(tag_string) + begin_offset
-    n = (begin_offset, end_offset)
+    n = (begin_offset, end_offset, img['src'])
     print(n)
     asset_offset_envelope[n] = [tag_string]
+    img['src'] = element2
+    changed_envelope[element2] = [img]
+    element2 += 1
 
 print(asset_offset_envelope)
+print(changed_envelope)
 
 # Step 3: Map all assets in the HTML to the location in the asset directory.
 # TODO: Replace pseudocode.
