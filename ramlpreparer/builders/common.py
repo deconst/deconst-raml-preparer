d2878fd80b70aaa3d3bb23599a571ec86b1e1481
Laura Santamaria <laura.santamaria@rackspace.com>
2017-06-12 10:21:01 -0500

    starting to clean up the copy-pasta; noting that it's modified from the Sphinx preparer's common file. Probably want to rename or consider rolling into something else because it's not really common across different builders since there's one builder...

--- ramlpreparer/builders/common.py
+++ ramlpreparer/builders/common.py
@@ -1,46 +1,43 @@
 # -*- coding: utf-8 -*-
 
-# From the Sphinx preparer's common.py
-#TODO: Update with the correct info vs copy-pasta
-#
-# """
-# Code shared between Builder implementations.
-# """
-#
-# import os
+# Modified from the Sphinx preparer's common.py
+# TODO: Update with the correct info vs copy-pasta
+
+'''
+Some minor set-up stuff
+'''
+
+import os
 # import glob
-# from os import path
-#
-# from deconstrst.config import Configuration
-# from deconstrst.builders.writer import OffsetHTMLTranslator
-#
-#
-# def init_builder(builder):
-#     """
-#     Common Builder initialization.
-#     """
-#
-#     builder.translator_class = OffsetHTMLTranslator
-#
-#     builder.deconst_config = Configuration(os.environ)
-#
-#     if path.exists('_deconst.json'):
-#         with open('_deconst.json', 'r', encoding='utf-8') as cf:
-#             builder.deconst_config.apply_file(cf)
-#
-# def derive_content_id(deconst_config, docname):
-#     """
-#     Consistently generate content IDs from document names.
-#     """
-#
-#     dirname, basename = path.split(docname)
-#     if basename == 'index':
-#         content_id_suffix = dirname
-#     else:
-#         content_id_suffix = docname
-#
-#     content_id = path.join(deconst_config.content_id_base, content_id_suffix)
-#     if content_id.endswith('/'):
-#         content_id = content_id[:-1]
-#
-#     return content_id
+from os import path
+
+from ramlpreparer.config import Configuration
+
+
+def init_builder(builder):
+    '''
+    Common Builder initialization.
+    '''
+    builder.deconst_config = Configuration(os.environ)
+
+    if path.exists('_deconst.json'):
+        with open('_deconst.json', 'r', encoding='utf-8') as cf:
+            builder.deconst_config.apply_file(cf)
+
+
+def derive_content_id(deconst_config, docname):
+    '''
+    Consistently generate content IDs from document names.
+    '''
+
+    dirname, basename = path.split(docname)
+    if basename == 'index':
+        content_id_suffix = dirname
+    else:
+        content_id_suffix = docname
+
+    content_id = path.join(deconst_config.content_id_base, content_id_suffix)
+    if content_id.endswith('/'):
+        content_id = content_id[:-1]
+
+    return content_id
