c3e7a975c495ef79a0bb3515f29fba479ad493a3
Laura Santamaria <laura.santamaria@rackspace.com>
2017-06-12 14:29:16 -0500

    starting a test for the config file

---
+++ tests/test_config.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+'''
+test_envelope_writer
+----------------------------------
+Tests for `envelope_writer` module.
+'''
+
+import unittest
+import subprocess
+import sys
+import os
+import json
+from os import path
+from bs4 import BeautifulSoup
+
+sys.path.append(path.join(path.dirname(__file__), '..'))
+
+from ramlpreparer.config import _normalize
+from ramlpreparer.config import Configuration
+
+# Initialize the raml2html package.
+starter_call = path.join(os.getcwd(), 'ramlpreparer',
+                         'scripts', 'npminstall.sh')
+subprocess.call(starter_call, shell=True)
+
+
+class ConfigTestCase(unittest.TestCase):
+    '''
+    Tests for the config method
+    '''
+
+    def test__normalize_pass(self):
+        '''
+        Question?
+        '''
+        pass
+
+    def test__normalize_fail(self):
+        '''
+        Question?
+        '''
+        pass
+
+
+class ConfigurationTestCase(unittest.TestCase):
+    '''
+    Tests for class methods in the Configuration class
+    '''
+
+    def setUp(self):
+        '''
+        Instantiate the class.
+        '''
+        self.config_class = Configuration()
+
+    def tearDown(self):
+        pass
+
+    def test_apply_file_pass(self):
+        '''
+        Question?
+        '''
+        pass
+
+    def test_apply_file_fail(self):
+        '''
+        Question?
+        '''
+        pass
+
+    @unittest.skip("feature not ready")
+    def test__get_git_root_pass(self):
+        '''
+        Question?
+        '''
+        pass
+
+    @unittest.skip("feature not ready")
+    def test__get_git_root_fail(self):
+        '''
+        Question?
+        '''
+        pass
+
+    @unittest.skip("feature not ready")
+    def test_missing_values_pass(self):
+        '''
+        Question?
+        '''
+        pass
+
+    @unittest.skip("feature not ready")
+    def test_missing_values_fail(self):
+        '''
+        Question?
+        '''
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
