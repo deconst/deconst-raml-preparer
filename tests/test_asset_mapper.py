6ec65b40a7c3060d1b6ccf34a690515f589712a5
Laura Santamaria <laura.santamaria@rackspace.com>
2017-06-12 10:51:39 -0500

    fixing the asset mapper test now that the method uses os path join

--- tests/test_asset_mapper.py
+++ tests/test_asset_mapper.py
@@ -18,7 +18,8 @@ sys.path.append(path.join(path.dirname(__file__), '..'))
 from ramlpreparer.builders.asset_mapper import map_the_assets
 
 # Initialize the raml2html package.
-starter_call = os.getcwd() + '/ramlpreparer/scripts/npminstall.sh'
+starter_call = path.join(os.getcwd(), 'ramlpreparer',
+                         'scripts', 'npminstall.sh')
 subprocess.call(starter_call, shell=True)
 
 
@@ -31,12 +32,15 @@ class AssetMapperTestCase(unittest.TestCase):
         '''
         Does the mapper successfully map the new path?
         '''
-        expected_version = {os.getcwd() + '/tests/dest/assets/tool_time12.jpg': 3980,
-                            os.getcwd() + '/tests/dest/assets/tool_time.jpg': 663}
-        source_assets = os.getcwd() + '/tests/src/assets/'
-        dest_assets = os.getcwd() + '/tests/dest/assets/'
+        tool_time12_path = path.join(
+            os.getcwd(), 'tests', 'dest', 'assets', 'tool_time12.jpg')
+        tool_time_path = path.join(
+            os.getcwd(), 'tests', 'dest', 'assets', 'tool_time.jpg')
+        expected_version = {tool_time12_path: 3980, tool_time_path: 663}
+        source_assets = path.join(os.getcwd(), 'tests', 'src', 'assets', '')
+        dest_assets = path.join(os.getcwd(), 'tests', 'dest', 'assets', '')
         x, mapped_version = map_the_assets(
-            '/tests/src/tester-raw.html', source_assets, dest_assets)
+            'tests/src/tester-raw.html', source_assets, dest_assets)
         self.assertEqual(expected_version, mapped_version)
 
     # def test_map_the_assets_asset_offset_fail(self):
