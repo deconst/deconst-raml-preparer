e0120e1ebae7b31f12923b560c4fc7c3ccceadee
Laura Santamaria <laura.santamaria@rackspace.com>
2017-06-13 10:44:56 -0500

    adding coverage checks

---
+++ tests/test_all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import test_asset_mapper
+import test_common
+import test_config
+import test_envelope_writer
+import test_raml2html
+import test_tocbuilder
+
+test_asset_mapper
+test_common
+test_config
+test_envelope_writer
+test_raml2html
+test_tocbuilder
