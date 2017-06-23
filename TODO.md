e83a00f8c0b4a63ecb56f9418f7b3320f8a1fa56
Laura Santamaria <laura.santamaria@rackspace.com>
2017-06-23 11:42:17 -0500

    cleaning up an issue with open's x option

--- TODO.md
+++ TODO.md
@@ -2,4 +2,4 @@
 -   Add a way to add intro text or other information to the generated docs (maybe a way to pull in markdown? straight HTML?)
 -   Use Jinja2 template to run the TOC generator
 -   Figure out how metadata can be added to the RAML spec's documentation
-*NOTE:* Look at envelopes with http://localhost:9000/content/https%3A%2F%2Fgithub.com%2F<org>%2F<repo>%2F<permalink> from the integrated deconst.
+*NOTE:* Look at envelopes with localhost:9000/content/https%3A%2F%2Fgithub.com%2F<org>%2F<repo>%2F<permalink> from the integrated deconst.
--- ramlpreparer/builders/raml2html.py
+++ ramlpreparer/builders/raml2html.py
@@ -18,29 +18,30 @@ def raml2html(file_raml, output_html):
         raise TypeError("This isn't RAML! Pass a RAML file to this method.")
     else:
         try:
-            output_file = open(output_html, 'x')
+            new_file = open(output_html, 'x')
+            new_file.close()
         except FileExistsError:
-            output_file = open(output_html, 'r')
-        scripting = os.path.join(
-            os.getcwd(),
-            'ramlpreparer',
-            'scripts',
-            'ramlconvert.sh'
-        )
-        nunjucks_path = os.path.join(
-            os.getcwd(),
-            'ramlpreparer',
-            'nunjucks',
-            'index.nunjucks'
-        )
-        htmlifyit = subprocess.call([
-            scripting,
-            file_raml,
-            output_html,
-            nunjucks_path
-        ])
-        apiraw = str(output_file.read())
-        output_file.close()
+            pass
+        with open(output_html, 'r') as output_file:
+            scripting = os.path.join(
+                os.getcwd(),
+                'ramlpreparer',
+                'scripts',
+                'ramlconvert.sh'
+            )
+            nunjucks_path = os.path.join(
+                os.getcwd(),
+                'ramlpreparer',
+                'nunjucks',
+                'index.nunjucks'
+            )
+            htmlifyit = subprocess.call([
+                scripting,
+                file_raml,
+                output_html,
+                nunjucks_path
+            ])
+            apiraw = str(output_file.read())
         return apiraw
 
 
